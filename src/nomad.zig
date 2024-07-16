1095312fb757dc69c97d062832bbb5df5b596b3a
vapandris <vapandris@gmail.com>
2024-07-16 17:37:35 +0200

    started implementation of smooth camera

--- src/nomad.zig
+++ src/nomad.zig
@@ -10,9 +10,12 @@ const NomadState = union(enum) {
     run,
 };
 
+const FaceDirection = union(enum) { up, down, left, right };
+
 pub const Nomad = struct {
     hitCircle: shapes.Circle,
     moveDirection: math.Vec2 = .{ .x = 0, .y = 0 },
+    faceDirection: FaceDirection = .down,
 
     frameCounter: u8 = 0,
     state: NomadState = .idle,
@@ -21,7 +24,30 @@ pub const Nomad = struct {
     const acceleration: f32 = 2.5;
     const deceleration = acceleration / 2;
 
+    pub fn getPositionForCameraToFollow(self: Nomad) math.Pos2 {
+        const faceing = self.faceDirection;
+        const distance: f32 = self.hitCircle.r * 2;
+        const dir = math.Pos2{
+            .x = distance * @as(f32, switch (faceing) {
+                .left => -1,
+                .right => 1,
+                else => 0,
+            }),
+            .y = distance * @as(f32, switch (faceing) {
+                .up => -1,
+                .down => 1,
+                else => 0,
+            }),
+        };
+
+        return .{
+            .x = self.hitCircle.pos.x + dir.x,
+            .y = self.hitCircle.pos.y + dir.y,
+        };
+    }
+
     pub fn update(self: *Nomad, frameDelta: f32) void {
+        // Update movement:
         self.moveDirection.normalize();
         const dir = self.moveDirection;
         const FPS = 60.0;
@@ -33,6 +59,10 @@ pub const Nomad = struct {
             maxSpeed,
             frameDelta,
         ) catch unreachable;
+
+        // Update face-direction:
+        self.faceDirection =
+            if (dir.x > 0) .right else if (dir.x < 0) .left else if (dir.y > 0) .down else if (dir.y < 0) .up else self.faceDirection;
     }
 
     pub fn draw(self: *Nomad) void {
@@ -70,5 +100,14 @@ pub const Nomad = struct {
             0,
             rl.Color.white,
         );
+
+        // Debug draw:
+        const posToFollow = self.getPositionForCameraToFollow();
+        const circle = assets.camera.ScreenCircleFromCircle(.{
+            .pos = posToFollow,
+            .r = 10,
+        }, screen.getScreenSize());
+
+        rl.drawCircle(circle.x, circle.y, circle.r, rl.Color.black);
     }
 };
